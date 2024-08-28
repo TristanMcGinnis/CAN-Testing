f601be3d126d5860bf1ed0b4fa79ecb619859cdc
Tristan McGinnis <tristanmcginnis26@gmail.com>
2024-08-28 10:38:06 -0500

    Added read/write to embedded scripts

--- embedded/Read/src/main.cpp
+++ embedded/Read/src/main.cpp
@@ -1,7 +1,42 @@
 #include <Arduino.h>
+#include <astraCAN.h>
+
+
+//Setting up for CAN0 line
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
+
+void readCan();
 
 void setup() {
+
+
+    // Initalization for using CAN with the sparkmax
+    Can0.begin();
+    Can0.setBaudRate(1000000);
+    Can0.setMaxMB(16);
+    Can0.enableFIFO();
+    Can0.enableFIFOInterrupt();
+
+
 }
 
 void loop() {
+  readCAN();
+  delay(50);
+}
+
+
+void readCAN(){
+  CAN_message_t msg;
+  while(Can0.read(msg)){
+    Serial.print("ID: ");
+    Serial.print(msg.id, HEX);
+    Serial.print(" ");
+    Serial.print("Data: ");
+    for(int i = 0; i < 8; i++){
+      Serial.print(msg.buf[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
 }
--- embedded/ReadWrite/src/main.cpp
+++ embedded/ReadWrite/src/main.cpp
@@ -1,8 +1,56 @@
 #include <Arduino.h>
+#include <astraCAN.h>
 
+
+//Setting up for CAN0 line
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
+
+void readCan();
+void sendAtInterval();
+unsigned long interal = 1000;
+unsigned long lastTime = 0;
 
 void setup() {
+
+
+    // Initalization for using CAN with the sparkmax
+    Can0.begin();
+    Can0.setBaudRate(1000000);
+    Can0.setMaxMB(16);
+    Can0.enableFIFO();
+    Can0.enableFIFOInterrupt();
+
+
 }
 
 void loop() {
+
+  readCAN();
+  delay(50);
+  sendAtInterval();
+  delay(50);
+}
+
+
+
+void sendAtInterval(){
+  if(millis() - lastTime > interal){
+    sendHeartbeat(Can0, 1);
+    lastTime = millis();
+  }
+}
+
+void readCAN(){
+  CAN_message_t msg;
+  while(Can0.read(msg)){
+    Serial.print("ID: ");
+    Serial.print(msg.id, HEX);
+    Serial.print(" ");
+    Serial.print("Data: ");
+    for(int i = 0; i < 8; i++){
+      Serial.print(msg.buf[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
 }
